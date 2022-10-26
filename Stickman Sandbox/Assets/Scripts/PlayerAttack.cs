068bb227a9892f06f991219a3d1850d3d33ea0dc
LewisCardy <91138837+LewisCardy@users.noreply.github.com>
2022-10-26 15:44:49 +0100

    2 punch system and comments

--- Stickman Sandbox/Assets/Scripts/PlayerAttack.cs
+++ Stickman Sandbox/Assets/Scripts/PlayerAttack.cs
@@ -2,24 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//The player attack script
 public class PlayerAttack : MonoBehaviour
 {
      Animator animator;
+
+    private float timeBetweenPunches = 0.0f;
+    private float timeStamp;
+    public static float time;
+    public float timeLastPass = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
+        //gets the animator component
         animator = GetComponent<Animator>();
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        Attack();
+        //checks for attack input each frame
+        AttackInput();
     }
 
-    void Attack(){
+    //checks for attack input
+    void AttackInput(){
+        //when E is pressed
         if (Input.GetKeyDown(KeyCode.E)){
-            animator.SetTrigger("Punch");
+            Attack();
+            
+            //current game time
+            time = Time.time;
+            //time since button press
+            float deltaTime = time - timeLastPass;
+            
+            //checks to see if e was pressed more than once in a certain amount of time
+            if (deltaTime > 5.0f){
+                //if the key was pressed more than once cross animation after jab
+                timeLastPass = time;
+                animator.SetTrigger("Cross"); 
+            } else {
+                //jab animation if pressed once
+                animator.SetTrigger("Jab");
+            }
+            
         }
     }
+    void Attack(){
+
+    }
+
 }
--- Stickman Sandbox/Assets/Scripts/PlayerMovement.cs
+++ Stickman Sandbox/Assets/Scripts/PlayerMovement.cs
@@ -4,46 +4,50 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Animator animator;
+    Animator animator; //animator
 
-    public float speed = 6f;
+    public float speed = 6f; //movement speed
 
-    float horizontalInput;
+    float horizontalInput; //horizontal keyboard input
 
-    Vector3 moveDirection;
+    Vector3 moveDirection; //move direction co ordinates
     
-    public float groundDrag;
+    public float groundDrag; //drag
     //Ground Check
-    public float playerHeight;
-    public LayerMask Ground;
-    bool grounded;
+    public float playerHeight; //player height
+    public LayerMask Ground; //ground layer mask
+    bool grounded; //if grounded
 
-    public KeyCode jump = KeyCode.Space;
+    public KeyCode jump = KeyCode.Space; //spacebar keycode for jump
 
-    Rigidbody rb;
-    public Transform orientation;
+    Rigidbody rb; //the rigidbody of the player
+    public Transform orientation; //orientation of the player
 
-    public float jumpForce;
-    public float jumpCooldown;
-    public float airMultiplier;
-    bool readyToJump;
+    public float jumpForce; //force of jump
+    public float jumpCooldown; //cooldown jump
+    public float airMultiplier; //air speed multiplier
+    bool readyToJump; //if ready to jump
 
-    private bool isJumping;
+    private bool isJumping; //if jumping
+
+
 
 
     private void Start() {
+        //gets the rigidbody for the object and freezes the rotation
         rb = GetComponent<Rigidbody>();
-        rb.freezeRotation = true;
+        rb.freezeRotation = true; 
         readyToJump = true;
-        animator = GetComponent<Animator>();
+        animator = GetComponent<Animator>(); // the animator component of the object
     }
     
     private void FixedUpdate(){
         Movement();
     }
 
-    void Update()
+    void Update() //updates every frame
     {
+        //creates a beam half the length of the player and if it touches an object with ground layer then the player is touching the ground
         grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, Ground);
         MyInput();
         if (horizontalInput > 0 || horizontalInput < 0){
@@ -53,28 +57,34 @@ public class PlayerMovement : MonoBehaviour
         }
         Debug.Log(grounded);
         if (grounded == true){
-            rb.drag = groundDrag;
+            rb.drag = groundDrag; //apply drag if ghrounded
+
+            //sets the animator to grounded and the rest to false when on the ground
             animator.SetBool("isGrounded", true);
             animator.SetBool("isJumping", false);
             animator.SetBool("isFalling", false);
         } else {
+            //animator not grounded
             animator.SetBool("isGrounded", false);
+            //if the y velocity is higher than 0 then jumping animation
             if (rb.velocity.y > 0){
                 animator.SetBool("isJumping", true);
             }
+            //if player is jumping and going down then fall animation
             if ((isJumping && rb.velocity.y < 0) || rb.velocity.y < -2){
                 animator.SetBool("isFalling", true);
             }
-
+            //0 drag in midair
             rb.drag = 0;
         }
         SpeedControl();
     }
 
 
-    private void MyInput(){
-        horizontalInput = Input.GetAxisRaw("Horizontal");
+    private void MyInput(){ //the keyboard inputs
+        horizontalInput = Input.GetAxisRaw("Horizontal"); //hoprizontal keyboard input
         
+        //if jumped and grounded and ready to jump then jump then reset jump
         if (Input.GetKey(jump) && readyToJump && grounded){
             readyToJump = false;
             isJumping = true;
@@ -83,34 +93,37 @@ public class PlayerMovement : MonoBehaviour
             
             Invoke(nameof(ResetJump), jumpCooldown);
         }
+
+
     }
 
-    private void Movement(){
-        moveDirection = orientation.right * horizontalInput;
+    private void Movement(){ //moves the player
+        moveDirection = orientation.right * horizontalInput; //calculates move direction
 
-        if (grounded){
+        if (grounded){ //if on the ground move in the direction with the speed of the player
             rb.AddForce(moveDirection.normalized * speed, ForceMode.Force);
             
-        } else if (!grounded){
+        } else if (!grounded){ //if not on the ground move with direction but with an added multiplier to move faster in the air
             rb.AddForce(moveDirection.normalized * speed * airMultiplier, ForceMode.Force);
         }
     }
 
-    private void SpeedControl(){
-        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.y);
-        if (flatVel.magnitude > speed) {
+    private void SpeedControl(){ //limits the players speed
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.y); //flat velocity of movement
+        if (flatVel.magnitude > speed) { //if moving faster than movespeed value then make a limited velocity making miove speed the maximum speed
             Vector3 limitedVel = flatVel.normalized * speed;
             rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
         }
     }
 
-    private void Jump(){
-        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    private void Jump(){ //jumping
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z); //reset y velocity
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse); //adds the juming force upwards on the y once.
 
     }
 
-    private void ResetJump(){
+    private void ResetJump(){ //resets jump
         readyToJump = true;
         
     }
